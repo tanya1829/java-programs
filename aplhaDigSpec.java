17518e1bb472a90728f290032c1777950e36f8e8
Tanya2918 <132035563+tanya1829@users.noreply.github.com>
2023-09-26 13:21:29 +0530

    Create aplhaDigSpec.java

---
+++ aplhaDigSpec.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class alphaDigSpec {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any character to check alphabet or not:");
+        char ch = sc.next().charAt(0);
+        int ascii =ch;
+
+        if ((ch>=65 && ch<=90) || (ch>=97 && ch<=122))
+            System.out.println(ch+" is alphabet.");
+        else if(ch>=48 && ch<=57)
+            System.out.println(ch+" is Digit.");
+        else
+            System.out.println(ch+" is Special Character");
+    }
+}
