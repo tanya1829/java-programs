f41550db1c0d3a2aea2786ccbb1342dc797bc464
Tanya2918 <132035563+tanya1829@users.noreply.github.com>
2023-09-26 11:38:49 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Test
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your first number:");
+		int a=sc.nextInt();
+		System.out.print("Enter your second number:");
+		int b=sc.nextInt();
+		System.out.println("Sum of a and b is"+a+b);	
+	}
+}
